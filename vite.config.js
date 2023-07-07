8bfeb5c8fe6308a6f05e7ce50f26bade504c54a0
ThomasStinkensPXL <12102615@student.pxl.be>
2023-07-07 12:52:09 +0200

    update dist

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/FavorCoolPages/" : "/",
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
